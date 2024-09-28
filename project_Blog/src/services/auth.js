f3b75b12ddf89db1c24e2a5074ee2e47e488e286
arjun pangeni <eemc365@gmail.com>
2024-09-28 23:15:06 +0545

    auth works

--- project_Blog/src/services/auth.js
+++ project_Blog/src/services/auth.js
@@ -1,3 +1,38 @@
+// const JWT = require('jsonwebtoken')
+
+// const secret = "chapakot123$$"
+
+// function createTokenForUser(user) {
+//     const payload = {
+//         _id: user.id,
+//         name: user.fullName,
+//         email: user.email,
+//         profileImageUrl: user.profileImageUrl,
+//         role: user.role
+//     };
+
+//     const token = JWT.sign(payload, secret)
+//     return token
+
+// }
+
+// function validateToken(token) {
+//     try {
+//         // Verify and return the decoded token payload
+//         const payload = JWT.verify(token, secret)
+//         return payload
+//     } catch (error) {
+//         // Handle token verification error (e.g., invalid or expired token)
+//         throw new Error("Invalid or expired token")
+//     }
+// }
+
+
+// module.exports = { createTokenForUser, validateToken }
+
+
+
+
 const JWT = require('jsonwebtoken')
 
 const secret = "chapakot123$$"
