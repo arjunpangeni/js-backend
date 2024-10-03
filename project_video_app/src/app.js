798961a9186f506261e62153bb8547405f02cd07
arjun pangeni <eemc365@gmail.com>
2024-10-04 03:49:02 +0545

    custom async handler funtion , which will be use for every asyn function

--- project_video_app/src/app.js
+++ project_video_app/src/app.js
@@ -3,4 +3,14 @@ import cors from 'cors'
 import 'cookieParser'
 const app = express();
 
-app.use(cors)
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.static('public'))
+app.use(cookieParser());
+
+export { app }
