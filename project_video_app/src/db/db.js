ffc336e31ec7978cb467ea2dd5c5292a9d55f968
arjun pangeni <eemc365@gmail.com>
2024-10-04 03:47:57 +0545

    database connncetion

---
+++ project_video_app/src/db/db.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+import { DB_Name } from '../constants.js'
+
+
+const dbConnect = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_Name}`)
+        console.log(`\n mongodb connected !! Db host: ${connectionInstance.connection.host}`)
+
+    } catch (error) {
+        console.log("mongodb connection error :", error)
+        process.exit(1)
+    }
+    /*Calling process.exit() immediately ends the Node.js process. 
+    It does not wait for any asynchronous operations to complete  */
+}
+
+
+
+export default dbConnect
