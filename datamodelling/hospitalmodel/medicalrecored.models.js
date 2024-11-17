00aef70523a520a277fde35874e0b48a881720a8
Arjun Pangeni <126581290+arjunpangeni@users.noreply.github.com>
2024-11-17 23:48:14 +0545

    Update medicalrecored.models.js

--- datamodelling/hospitalmodel/medicalrecored.models.js
+++ datamodelling/hospitalmodel/medicalrecored.models.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const recordSchema = new mongoose.Schema({
-
+user{}
 }, { timestamps: true })
 
 export const MedicalRecord = mongoose.model('MedicalRecord', recordSchema) 
